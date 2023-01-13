c07b66417e1d1ff18f2e0d7a7e85a8e62212d655
Muana <lalhming.muana1982@gmail.com>
2023-01-13 17:54:32 +0530

    smallest difference solution completed

--- 10-smallest difference.js
+++ 10-smallest difference.js
@@ -1,4 +1,32 @@
-let c=5;
-let d=-6
-const a= Math.abs(c-d);
-console.log(a);
+/**
+ * Given two non empty arrays. write a function to find a pair of numbers 
+ * (One from each array) whose absolute difference is closest to zero.
+ * 
+ * eg a1 = [1,2,4]
+ *    a2 = [2,1,3]  
+ * 
+ * answer [1,1]
+ * 
+ */
+
+
+
+function smallestDifference(arrayOne, arrayTwo) {
+    let value=Math.abs(arrayOne[0]-arrayTwo[0]);
+   let first = arrayOne[0];
+   let second = arrayTwo[0];
+   
+   for(let i=0; i<arrayOne.length; i++){
+       for(let j=0; j<arrayTwo.length; j++){
+           let a=Math.abs(arrayOne[i]-arrayTwo[j]);
+   
+           if(a<value){ 
+               value =a;
+               first = arrayOne[i];
+           second= arrayTwo[j];
+       }
+        
+       }
+   }
+     return [first,second];
+   }
