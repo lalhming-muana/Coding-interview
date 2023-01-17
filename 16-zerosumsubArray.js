d2620d6489b05d1b09350b3f850ad0bdd7fbec01
“Muana” <lalhming.muana1982@gmail.com>
2023-01-17 18:05:33 +0530

    zero sub array sum

---
+++ 16-zerosumsubArray.js
@@ -0,0 +1,36 @@
+/*
+
+https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+
+*/
+
+function zeroSumSubarray(nums) {
+    const result = new Set([0]);
+    let sum =0;
+    
+    for(const num of nums){
+        sum= sum+ num;
+        if(result.has(sum)){
+            return true;
+        }
+        result.add(sum);
+    }
+      return false;
+    }
+    
