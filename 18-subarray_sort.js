86e012ccca31f9581950a678eb883a99dff6c21d
“Muana” <lalhming.muana1982@gmail.com>
2023-01-19 12:52:33 +0530

    subarray sort first solution

--- 18-subarray_sort.js
+++ 18-subarray_sort.js
@@ -1,4 +1,6 @@
 /*
+O(NlogN)
+
 Given an integer array nums, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order.
 
 Return the shortest such subarray and output its length.
@@ -24,3 +26,34 @@ A good approach would be to use two pointers to check the start and end of the
 sub array to sort
 */
 
+var findUnsortedSubarray = function(nums) {
+
+    let b =[...nums]
+    b.sort((a,b)=>a-b);
+    console.log(nums);
+    console.log(b);
+    
+    let first = -1;
+    let second = -1;
+    
+    if(nums.length ===1) return 0;
+    
+    for(let i=0;i<b.length;i++){
+        
+        if((b[i] != nums[i]) && first === -1 && second === -1)
+        first =i;
+        
+        if(b[i] != nums[i] && first != -1){
+            if (second <= i) second =i;
+        }
+        
+    }
+    
+    if(first === -1 && second === -1)return 0;
+    result=nums.slice(first,second+1)
+    
+    
+    
+    return result.length;
+        
+    };
