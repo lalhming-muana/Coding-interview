f876e2a3c4d34f94c64d19fa5dfa878af61529e1
“Muana” <lalhming.muana1982@gmail.com>
2023-02-01 13:13:26 +0530

    min distances array for one req completed

--- Arrays/22_Apartment_hunting.js
+++ Arrays/22_Apartment_hunting.js
@@ -26,16 +26,41 @@ blocks =[{
 
 reqs =["gym","school","store"]
 
-console.log(blocks[1]["gym"])
+
 
 /*
 
+Here we have an array of objects, we first need to learn how to work with arrays of objects
 
-for(let i=0; i <Object.keys(blocks).length; i++){
-    
-console.log("Element of a hashtable " +Object.keys(blocks)[i]);
-console.log(blocks[Object.keys(blocks)[i]])
-
+https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
     
 }
 */
+
+
+
+let i=0
+let lastLocation = 0;   
+    for(let j=0; j<blocks.length; j++){
+        let count =0;
+       
+        let compare;
+        let currentBlock = blocks[j][reqs[i]];
+       // console.log('j=',j,' currentBlock=',currentBlock)
+        if(currentBlock){
+            gym[j] = count+0;
+            lastLocation=j
+        }
+        if(!currentBlock){
+            count++;
+            compare = j - lastLocation;
+            console.log('lastlocation',lastLocation,' j=',j,'compare',compare)
+            gym[j] =Math.max(compare,count);
+        }
+        
+    }
+    
+    
+
+
+console.log(gym)
